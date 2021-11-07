df203d7005d1a5bb4078449dd79725c30fb64484
Phuc <phuc.le01@sjsu.edu>
2021-11-06 20:34:34 -0700

    Implement front end

--- 131_S03-final/131_S03/myapp/routes.py
+++ 131_S03-final/131_S03/myapp/routes.py
@@ -1,45 +1,20 @@
 from myapp import myapp_obj
-from myapp.forms import LoginForm
+from myapp.forms import TopCities
 from flask import render_template, flash, redirect
 
 from myapp import db
-from myapp.models import User, Post
-from flask_login import current_user, login_user, logout_user, login_required
-
-@myapp_obj.route("/loggedin")
-@login_required
-def log():
-    return 'Hi you are logged in'
-
-@myapp_obj.route("/logout")
-def logout():
-    logout_user()
-    return redirect('/')
-
-@myapp_obj.route("/login", methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Login invalid username or password!')
-            return redirect('/login')
-        login_user(user, remember=form.remember_me.data)
-        flash(f'Login requested for user {form.username.data},remember_me={form.remember_me.data}')
-        flash(f'Login password {form.password.data}')
-        return redirect('/')
-    return render_template("login.html", form=form)
-
-@myapp_obj.route("/members/<string:name>/")
-def getMember(name):
-    return 'Hi ' + name
+from myapp.models import City
 
 @myapp_obj.route("/")
-def hello():
-    name = 'Thomas'
-    people = {'Carlos' : 54}
-    title = 'My HommePage'
-    posts = [{'author': 'john', 'body': 'Beautiful day in Portland!'},\
-            {'author': 'Susan', 'body': 'Today was a good day!'}]
+def begin():
+    return redirect("/home")
 
-    return render_template('hello.html', name=name, people=people, posts=posts)
+@myapp_obj.route("/home", methods=["GET", "POST"])
+def home():
+    form = TopCities()
+    if form.validate_on_submit():
+        new_city = City(name=form.city_name.data, rank=form.city_rank.data)
+        db.session.add(new_city)
+        db.session.commit()
+    posts = [{}]
+    return render_template('home.html', title = 'Top Cities', form = form, name = 'Phuc', posts = posts)
